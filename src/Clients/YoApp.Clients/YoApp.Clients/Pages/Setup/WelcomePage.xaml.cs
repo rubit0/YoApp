b63749dec63a5dbcbe3448eb2477b585debdb314
Ruben de la Torre <ruben@studio-delatorre.com>
2017-04-26 22:55:19 +0200

    Improve animations for Welcome- and EnterNumber page.

--- src/Clients/YoApp.Clients/YoApp.Clients/Pages/Setup/WelcomePage.xaml.cs
+++ src/Clients/YoApp.Clients/YoApp.Clients/Pages/Setup/WelcomePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Autofac;
 using Xamarin.Forms;
 using YoApp.Clients.Forms;
@@ -45,6 +46,12 @@ namespace YoApp.Clients.Pages.Setup
                 StartPagePresentation();
         }
 
+        protected override void OnDisappearing()
+        {
+            this.CancelAnimation();
+            base.OnDisappearing();
+        }
+
         protected override bool OnBackButtonPressed()
         {
             return true;
@@ -70,6 +77,22 @@ namespace YoApp.Clients.Pages.Setup
             SplashIcon.Opacity = 0;
             Overlay.Opacity = 0;
             SplashIcon.RotationY = -90;
+            IconsContainer.Spacing = -0;
+            IconsContainer.Opacity = 0;
+
+            var iconsAnimation = new Animation(v => IconsContainer.Spacing = v, 0, 30, Easing.CubicOut);
+            var iconsFade = new Animation(v => IconsContainer.Opacity = v);
+
+            var captions = CaptionsContainer.Children;
+            for (var i = 0; i < captions.Count; i++)
+            {
+                captions[i].Opacity = 0;
+                var iterator = i;
+                var fader = new Animation(v => captions[iterator].Opacity = v, 0, 1, Easing.CubicIn);
+                var begin = 0.25 + (i * 0.25);
+                var end = 0.6 + (i * 0.2);
+                animationController.Add(begin, end, fader);
+            }
 
             var slideInSecond = new Animation(
                 v => SlideSecond.TranslationY = Lerp(-layoutHeight, layoutHeight, v), 
@@ -106,6 +129,8 @@ namespace YoApp.Clients.Pages.Setup
             animationController.Add(0.3, 0.7, fadeInIcon);
             animationController.Add(0.5, 1, rotateIcon);
             animationController.Add(0, 0.5, fadeInOverlay);
+            animationController.Add(0.5, 1, iconsAnimation);
+            animationController.Add(0.4, 0.6, iconsFade);
 
             animationController.Add(0.2, 0.8, slideInSecond);
             animationController.Add(0.1, 0.8, rotateSecond);
